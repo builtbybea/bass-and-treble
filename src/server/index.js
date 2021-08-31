41ee30f05e816c55384dde4d62f2eaee34ac8425
Bea <38406219+builtbybea@users.noreply.github.com>
2021-08-31 17:22:17 +0100

    set up server

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+const express = require("express"); // importing
+
+const app = express(); // creating an app, which is the server
+const PORT = 8000; // which port the server will run on
+
+app.get("/", (req, res) => res.send("Express + TypeScript Server")); //defining your handler
+app.get("/api/concerts", (req, res) => res.send("list of concerts"));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
