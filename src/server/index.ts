fb9eeae04d1b5543c209484246bb8d73836cec65
Bea <38406219+builtbybea@users.noreply.github.com>
2021-09-02 15:17:25 +0100

    wip: sent JSON data back to Express

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,8 +3,25 @@ import express from "express"; // importing
 const app = express(); // creating an app, which is the server
 const PORT = 8000; // which port the server will run on
 
+const concert = [
+  {
+    artist: "Disclourse",
+    date: "05/03/2022",
+  },
+  {
+    artist: "Erika De Casier",
+    date: "11/11/2021",
+  },
+  {
+    artist: "Rina Sawayana",
+    date: "17/11/2021",
+  },
+];
+
+// https://stackoverflow.com/questions/39301227/external-api-calls-with-express-node-js-and-require-module
+
 app.get("/", (req, res) => res.send("Express + TypeScript Server")); //defining your handler
-app.get("/api/concerts", (req, res) => res.send("changes"));
+app.get("/api/concerts", (req, res) => res.json(concert));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
