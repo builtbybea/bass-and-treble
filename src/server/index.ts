19eb08b634288e80e57c7cbaec64123fdf77bf3d
Bea <38406219+builtbybea@users.noreply.github.com>
2021-09-06 00:06:20 +0100

    wip: retrieve postcode from query parms and returned to browser

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,26 +6,16 @@ const app = express(); // creating an app, which is the server
 const PORT = 8000; // which port the server will run on
 
 const postCode = "SW10 0EF";
-const concert = [
-  {
-    artist: "Disclourse",
-    date: "05/03/2022",
-  },
-  {
-    artist: "Erika De Casier",
-    date: "11/11/2021",
-  },
-  {
-    artist: "Rina Sawayana",
-    date: "17/11/2021",
-  },
-];
 
 const API_KEY = process.env.TICKET_MASTER_API_KEY;
 const city = "London";
 
 app.get("/", (req, res) => res.send("Express + TypeScript Server")); //defining your handler
-app.get("/api/concerts", (req, res) => res.json(concert));
+app.get("/api/concerts", (req, res) => {
+  const postcode = req.query.postcode;
+
+  res.send(postcode);
+});
 
 app.get("/api/postcode", (req, res) => {
   axios
