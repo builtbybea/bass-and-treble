40bd1cbf4f7db1364ec51a707c718da0fddf4631
Bea <38406219+builtbybea@users.noreply.github.com>
2021-09-13 21:31:37 +0100

    refactor: added typing and added generic to promise

--- src/server/api/data.ts
+++ src/server/api/data.ts
@@ -10,6 +10,10 @@ interface PostcodeResponse {
   result: { latitude: number; longitude: number };
 }
 
+interface TicketmasterResponse {
+  _embedded: any;
+}
+
 const API_KEY = process.env.TICKET_MASTER_API_KEY;
 
 export const getCoordinates = async (
@@ -32,8 +36,8 @@ export const getNearestConcert = async (
   radius: number,
   unit: string,
   geohash: any
-): Promise<{}> => {
-  const response = await axios.get(
+): Promise<TicketmasterResponse> => {
+  const response = await axios.get<TicketmasterResponse>(
     `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}&radius=${radius}&unit=${unit}&geoPoint=${geohash}`
   );
   const concertDetails = response.data._embedded;
